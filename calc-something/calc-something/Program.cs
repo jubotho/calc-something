2b978956176d0a14bb288139f8256ee3095cfa2f
Svetoslav Mitov <iwm2stuff@gmail.com>
2025-02-21 18:28:53 +0200

    Implement ask and reply name

--- calc-something/calc-something/Program.cs
+++ calc-something/calc-something/Program.cs
@@ -5,5 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World asdd dsads ad sa!");
+
+        string name = Console.ReadLine();
+        
+        Console.WriteLine($"Hello, {name}!");
     }
 }
